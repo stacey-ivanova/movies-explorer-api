1432e0e02330fd6aa5aa53147b52fbc0110113b3
Ivanova Stacey <stacey_ivanova@mail.ru>
2022-10-29 03:57:01 +0300

    second version

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -2,7 +2,7 @@ const { messages } = require('../utils/constants');
 
 module.exports.errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(err.statusCode).send({
+  res.status(statusCode).send({
     message: statusCode === 500
       ? messages[500].default
       : message,
